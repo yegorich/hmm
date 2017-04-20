ecd67bf9f7a0d3b0e1c2a8e8f98056d860c4203b
Noam Finkelstein <n-s-f@users.noreply.github.com>
2017-04-20 18:47:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hmm',
     version='0.0.1',
-    description='Functions for inference on HMMs',
+    description='Functions for working with HMMs',
     url='https://github.com/n-s-f/hmm',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
 )
